5fe8b08b5e3bd2f2bf94446432b302eec3b24b2b
Ayrton Guttier <ayrtonsg@hotmail.com>
2024-05-29 19:28:16 -0300

    copilot

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -22,6 +22,7 @@ local plugins = {
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/nvim-cmp' },
     { 'L3MON4D3/LuaSnip' },
+    { 'github/copilot.vim' },
     -- folke
     "folke/neodev.nvim",
     -- treesitter
