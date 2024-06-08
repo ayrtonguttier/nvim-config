78694f3ff0c2e7e13073801847c703842901d4b3
Ayrton Guttier <ayrtonsg@hotmail.com>
2024-06-08 00:50:04 -0300

    undotree

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,5 +6,6 @@ return {
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fs', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 	end
 }
