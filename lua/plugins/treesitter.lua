b0819c0625f7921472ea6d422a0e9b70690552af
Ayrton Guttier <ayrtonsg@hotmail.com>
2024-06-08 02:02:18 -0300

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,16 @@
 return {
-	{ "nvim-treesitter/nvim-treesitter", build="TSUpdate" }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = "TSUpdate",
+        config = function()
+            print('config treesitter')
+            local configs = require('nvim-treesitter.configs')
+            configs.setup({
+                auto_install = true,
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    }
 }
